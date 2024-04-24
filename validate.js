e7e4871831390a32ceb254925052f14c06bc63f6
stfnrosa <rosaa.estefanii@gmail.com>
2024-04-24 17:39:20 -0300

    finalizado para desktop

--- validate.js
+++ validate.js
@@ -1,1 +1,28 @@
-//Seu JavaScript de validação aqui
+let inputNome = document.getElementById("nome");
+let inputEmail = document.getElementById("email");
+let inputAssunto = document.getElementById("assunto");
+let inputMensagem = document.getElementById("mensagem");
+
+function validarFormulario() {
+  if (inputNome.value == "") {
+    alert('O campo "Nome" não pode ser vazio');
+    return false;
+  }
+
+  if (inputEmail.value == "") {
+    alert('O campo "Email" não pode ser vazio');
+    return false;
+  }
+
+  if (inputAssunto.value == "") {
+    alert('O campo "Assunto" não pode ser vazio');
+    return false;
+  }
+
+  if (inputMensagem.value == "") {
+    alert('O campo "Mensagem" não pode ser vazio');
+    return false;
+  }
+
+  return true;
+}
